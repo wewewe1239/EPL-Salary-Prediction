89e58b0d4b4f6fe852af1e62e4ebe9d8969dd88a
amnayk <amnayk@gmail.com>
2020-10-22 17:39:38 +0200

    comment the functionnal test to avoid unnecessary writing

--- src/tests/test_merge_datasets_functionnal.py
+++ src/tests/test_merge_datasets_functionnal.py
@@ -2,8 +2,8 @@ import unittest
 from merge_datasets import merge_stats_and_salaries
 
 
-class TestFunctionnal(unittest.TestCase):
-    def test_merge_stats_and_salaries_should_return_datasets_with_no_NaNs(self):
-        df = merge_stats_and_salaries()
-        assert(len(df) > 400 and ~(df.isnull().values.any()))
+# class TestFunctionnal(unittest.TestCase):
+#     def test_merge_stats_and_salaries_should_return_datasets_with_no_NaNs(self):
+#         df = merge_stats_and_salaries()
+#         assert(len(df) > 400 and ~(df.isnull().values.any()))
 
